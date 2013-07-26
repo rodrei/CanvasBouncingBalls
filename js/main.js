6ddd355e896b87e38ff1ebd6b8e3ee20d89d3851
rodrei <rodrigopavano@gmail.com>
2013-07-26 00:45:02 -0700

    Refatored: moved events setup to App method

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+$(document).ready( function() {
+  App.init();
+
+  Stage.init({
+    canvas: canvas,
+    objects: ballsGenerator.run()
+  });
+
+  setTimeout(Stage.animate(), 300);
+});
+
+
 window.requestAnimFrame = (function(callback) {
   return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
     function(callback) {
@@ -5,35 +17,64 @@ window.requestAnimFrame = (function(callback) {
   };
 })();
 
+
 var App = function(){
   var canvas;
   var init = function(){
     App.canvas = document.getElementById('canvas');
     App.canvas.width = window.innerWidth;
     App.canvas.height = window.innerHeight - 41;
-  }
+    setupEvents();
+  };
+
+  var setupEvents = function(){
+    $('#reset_button').click(function() {
+      Stage.objects = ballsGenerator.run();
+      Stage.objects.first
+    });
+
+    $('#gravity_slider').slider({
+      change: function(event, ui){
+        Stage.gravity = ui.value;
+      },
+      value: 980,
+      max: 3000,
+      min: 700
+    });
+
+    $('#bounciness_slider').slider({
+      change: function(event, ui){
+        ballsGenerator.options['maxBounceFactor'] = ui.value / 100;
+        for (var i = 0; i < Stage.objects.length; i++) {
+          Stage.objects[i].maxBounceFactor = ui.value / 100;
+        }
+      },
+      value: 80,
+      max: 98,
+      min: 10
+    });
+  };
+
   return {
     canvas: canvas,
     init: init
   }
 }();
 
+
 var ballsGenerator = function(){
-  var options = {}
+  var options = {};
 
   var run = function(){
-    options = options || {};
-    var width =  App.canvas.width;
-    var height = App.canvas.height;
-
-    var maxDistanceBetween = 40;
-    var minDistanceBetween = 5;
-    var maxRadius = 40;
-    var minRadius = 5;
-    var colors = ['E53481', 'FCB215', '9CCB3B', '25B0E6', '8151A1'];
-
-    var balls = []
-    var offset = 0;
+    var width =  App.canvas.width,
+        height = App.canvas.height,
+        maxDistanceBetween = 40,
+        minDistanceBetween = 5,
+        maxRadius = 40,
+        minRadius = 5,
+        colors = ['E53481', 'FCB215', '9CCB3B', '25B0E6', '8151A1'],
+        balls = [],
+        offset = 0;
 
     while(true){
       var newBall = new Ball({
@@ -41,10 +82,10 @@ var ballsGenerator = function(){
         fillColor: colors[Util.randomIntBetween(0, colors.length)]
       });
 
-      distanceBetween = Util.randomBetween(minDistanceBetween, maxDistanceBetween);
+      var distanceBetween = Util.randomBetween(minDistanceBetween, maxDistanceBetween);
       newBall.x = offset + distanceBetween + newBall.radius;
       newBall.y = Util.randomBetween((height - newBall.radius), newBall.radius);
-      if(options['maxBounceFactor'] !== undefined){
+      if(ballsGenerator.options['maxBounceFactor'] !== undefined){
         newBall.maxBounceFactor = options['maxBounceFactor']
       }
 
@@ -65,48 +106,6 @@ var ballsGenerator = function(){
 }();
 
 
-$(document).ready( function() {
-  App.init();
-
-  //--------------------------------------------------
-  //Animation
-  //--------------------------------------------------
-  Stage.init({
-    canvas: canvas,
-    objects: ballsGenerator.run()
-  });
-
-  setTimeout(Stage.animate(), 300);
-
-  $('#reset_button').click(function() {
-    Stage.objects = ballsGenerator.run();
-    Stage.objects.first
-  });
-
-  //--------------------------------------------------
-  //Sliders
-  //--------------------------------------------------
-  $('#gravity_slider').slider({
-    change: function(event, ui){
-      Stage.gravity = ui.value;
-    },
-    value: 980,
-    max: 3000,
-    min: 700
-  });
-
-  $('#bounciness_slider').slider({
-    change: function(event, ui){
-      ballsGenerator.options['maxBounceFactor'] = ui.value / 100;
-      for (var i = 0; i < Stage.objects.length; i++) {
-        Stage.objects[i].maxBounceFactor = ui.value / 100;
-      }
-    },
-    value: 80,
-    max: 98,
-    min: 10
-  });
-});
 
 
 
